46b0b68fb81abcbfee867aed81da82ba0b26ca51
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-16 10:10:12 +0900

    vue cli exercise

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div> hi hello </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 컴포넌트를 공유했을때 데이터 공유가되지 않도록 새객체를 반환하는 형식으로 작성한다
+// new Vue({
+//   data: function() {
+//     return {
+//       str: 'hi'
+//     }
+//   }
+// })
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data:function() {
+    return {
+      str: 'hi'
+    }
   }
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
