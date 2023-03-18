43f804868e0742bf82d0599a1fffda3b7235219e
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-18 20:45:55 +0900

    뷰 컴포넌트생성, props, event

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div> hi hello </div>
+  <div>
+    <app-header
+     v-bind:propsdata="headerProps"
+     v-on:renew="changeHeader">
+    </app-header>
+  </div>
 </template>
 
 
 <script>
+import AppHeader from './components/AppHeader.vue';
 // 컴포넌트를 공유했을때 데이터 공유가되지 않도록 새객체를 반환하는 형식으로 작성한다
 // new Vue({
 //   data: function() {
@@ -16,7 +22,15 @@
 export default {
   data:function() {
     return {
-      str: 'hi'
+      headerProps: 'header 입니다.'
+    }
+  },
+  components: {
+    'app-header' : AppHeader
+  },
+  methods: {
+    changeHeader: function() {
+      this.headerProps = 'hi header 입니다.';
     }
   }
 }
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header><h1>{{ propsdata }}</h1></header>
+    <button v-on:click="sendEvent">send</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['propsdata'],
+  methods: {
+    sendEvent: function() {
+      this.$emit('renew');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
